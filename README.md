57ca42bed2fd75f0e34a4978a783c012e7c56d19
Art Vardanyan <akvardanyan99@gmail.com>
2025-06-25 05:58:20 -0400

    wip

---
+++ README.md
@@ -0,0 +1,1 @@
+Welcome to the rename_cover_letters NLP script!
--- rename_cover_letters.py
+++ rename_cover_letters.py
@@ -1,1 +1,7 @@
 import spacy
+import re
+from pathlib import Path
+from PyPDF2 import PdfReader
+
+nlp = spacy.load("en_core_web_sm")
+
